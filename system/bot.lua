e2131434999d75f4cc6c835a097441bb254f6eda
Reza1500 <rmohammadnia1378@gmail.com>
2016-11-22 15:03:41 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,7 +234,7 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {132875232},--Sudo users
+    sudo_users = {184905857,152513900},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
